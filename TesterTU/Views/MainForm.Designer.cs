f913bbabf9a0c1baae727619e21a7714757d2218
dubina <artemiusgrand@gmail.com>
2022-03-22 15:15:14 +0300

    rename Title in Form

--- TesterTU/Views/MainForm.Designer.cs
+++ TesterTU/Views/MainForm.Designer.cs
@@ -32,19 +32,18 @@ namespace TesterTU.Views
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.buttonStartStop = new System.Windows.Forms.Button();
             this.panelSettings = new System.Windows.Forms.Panel();
+            this.panelDevices = new System.Windows.Forms.Panel();
             this.settingsPanelMK2 = new TesterTU.Views.SettingsPanel();
             this.settingsPanelMK1 = new TesterTU.Views.SettingsPanel();
-            this.panelDevices = new System.Windows.Forms.Panel();
             this.panelSettings.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonStartStop
             // 
             this.buttonStartStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonStartStop.Location = new System.Drawing.Point(629, 41);
-            this.buttonStartStop.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonStartStop.Location = new System.Drawing.Point(472, 33);
             this.buttonStartStop.Name = "buttonStartStop";
-            this.buttonStartStop.Size = new System.Drawing.Size(177, 66);
+            this.buttonStartStop.Size = new System.Drawing.Size(133, 54);
             this.buttonStartStop.TabIndex = 0;
             this.buttonStartStop.Text = "СТАРТ";
             this.buttonStartStop.UseVisualStyleBackColor = true;
@@ -59,46 +58,49 @@ namespace TesterTU.Views
             this.panelSettings.Controls.Add(this.buttonStartStop);
             this.panelSettings.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelSettings.Location = new System.Drawing.Point(0, 0);
+            this.panelSettings.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panelSettings.Name = "panelSettings";
-            this.panelSettings.Size = new System.Drawing.Size(1525, 146);
+            this.panelSettings.Size = new System.Drawing.Size(1144, 119);
             this.panelSettings.TabIndex = 7;
+            // 
+            // panelDevices
+            // 
+            this.panelDevices.AutoScroll = true;
+            this.panelDevices.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDevices.Location = new System.Drawing.Point(0, 119);
+            this.panelDevices.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelDevices.Name = "panelDevices";
+            this.panelDevices.Size = new System.Drawing.Size(1144, 576);
+            this.panelDevices.TabIndex = 8;
             // 
             // settingsPanelMK2
             // 
-            this.settingsPanelMK2.Location = new System.Drawing.Point(310, 6);
+            this.settingsPanelMK2.Location = new System.Drawing.Point(232, 5);
+            this.settingsPanelMK2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.settingsPanelMK2.Name = "settingsPanelMK2";
-            this.settingsPanelMK2.Size = new System.Drawing.Size(298, 130);
+            this.settingsPanelMK2.Size = new System.Drawing.Size(224, 106);
             this.settingsPanelMK2.TabIndex = 2;
             this.settingsPanelMK2.TextPanel = "Настройка COM МК2";
             // 
             // settingsPanelMK1
             // 
             this.settingsPanelMK1.Location = new System.Drawing.Point(0, 0);
+            this.settingsPanelMK1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.settingsPanelMK1.Name = "settingsPanelMK1";
-            this.settingsPanelMK1.Size = new System.Drawing.Size(298, 135);
+            this.settingsPanelMK1.Size = new System.Drawing.Size(224, 110);
             this.settingsPanelMK1.TabIndex = 1;
             this.settingsPanelMK1.TextPanel = "Настройка COM МК1";
             // 
-            // panelDevices
-            // 
-            this.panelDevices.AutoScroll = true;
-            this.panelDevices.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelDevices.Location = new System.Drawing.Point(0, 146);
-            this.panelDevices.Name = "panelDevices";
-            this.panelDevices.Size = new System.Drawing.Size(1525, 709);
-            this.panelDevices.TabIndex = 8;
-            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1525, 855);
+            this.ClientSize = new System.Drawing.Size(1144, 695);
             this.Controls.Add(this.panelDevices);
             this.Controls.Add(this.panelSettings);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
-            this.Text = "Тестер ТУ";
+            this.Text = "Тестер КУС";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panelSettings.ResumeLayout(false);
             this.ResumeLayout(false);
